f5ebd4bb96a000c6cb1b62fa2b058ac025767015
im1sha <mikefoxx2000@ya.ru>
2019-06-07 20:59:21 +0300

    Base of project was created

---
+++ Export/Export.Test/Core.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Export.Test
+{
+    [TestClass]
+    public class Core
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Export/Export.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Export.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Export.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("4645e708-3d5d-4b66-b223-1a9d8f9147ef")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
