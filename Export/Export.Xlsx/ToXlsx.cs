5762d9e297d6fc537f2eda0e21b39d96a7306e40
boika.kv <boika.kv@gmail.com>
2019-06-10 17:26:34 +0300

    Add IFormatBuilder

--- Export/Export.Xlsx/ToXlsx.cs
+++ Export/Export.Xlsx/ToXlsx.cs
@@ -4,10 +4,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Export;
 
 namespace Export.Xlsx
 {
-    public class ToXlsx<T>
+    public class ToXlsx<T> : IFormatBuilder<T>
     {
         public void Export(T item)
         {
