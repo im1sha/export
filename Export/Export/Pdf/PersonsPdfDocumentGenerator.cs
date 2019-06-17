3e9a31b86a6273273fb41329469d5bc9d32401e6
im1sha <mikefoxx2000@ya.ru>
2019-06-17 15:21:00 +0300

    Partial implementation for Pdf. Add dependencies

---
+++ Export/Export/Pdf/PersonsPdfDocumentGenerator.cs
@@ -0,0 +1,18 @@
+﻿using Export.Base;
+using Export.DataStructures;
+using System.Collections.Generic;
+
+namespace Export.Pdf
+{
+    /// <summary>
+    /// Generates content of type <see cref="Person"/> to .pdf-document using the given structure representation (<see cref="IPdfDocumentBuilder"/>)  
+    /// </summary>
+    public class PersonsPdfDocumentGenerator : DocumentGenerator<IPdfDocumentBuilder, IEnumerable<Person>>
+    {
+        public PersonsPdfDocumentGenerator(IPdfDocumentBuilder documentBuilder,
+            IDocumentContentMapper<IPdfDocumentBuilder, IEnumerable<Person>> documentContentMapper) : base(
+                documentBuilder, documentContentMapper)
+        {
+        }
+    }
+}
