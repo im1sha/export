6eaa4ef08d1ba2004906de3ed1d8f4d8f9bc1a6c
boika.kv <boika.kv@gmail.com>
2019-06-19 15:50:57 +0300

    Edit XlsxDocumentBuilder

--- Export/Export/Xlsx/XlsxDocumentBuilder.cs
+++ Export/Export/Xlsx/XlsxDocumentBuilder.cs
@@ -12,11 +12,11 @@ namespace Export.Xlsx
     {
         //private readonly List<string> _text;
 
-        private readonly string[,] _text;
+        private readonly List<(int rowIndex, int columnIndex, string value)> _text;
 
         public XlsxDocumentBuilder()
         {
-            _text = new string[2,2];
+            _text = new List<(int rowIndex, int columnIndex, string value)>();
         }
 
         public void ToStream(Stream stream)
@@ -25,18 +25,23 @@ namespace Export.Xlsx
             {
                 var worksheet = workbook.Worksheets.Add("List_1");
 
-                int row = 0;
-                for (int i = 1; i < _text.Length - 1; i++)
+                //int row = 0;
+                //for (int i = 1; i < _text.Count - 1; i++)
+                //{
+                //    for (int j = 1; j <= i; j++)
+                //    {
+                //        int colum = 0;
+                //        worksheet.Cell(i, j).Value = _text[row, colum];
+                //        colum++;
+                //        j++;
+                //        worksheet.Cell(i, j).Value = _text[row, colum];
+                //        row++;
+                //    }
+                //}
+
+                for (int i = 0; i < _text.Count; i++)
                 {
-                    for (int j = 1; j <= i; j++)
-                    {
-                        int colum = 0;
-                        worksheet.Cell(i, j).Value = _text[row, colum];
-                        colum++;
-                        j++;
-                        worksheet.Cell(i, j).Value = _text[row, colum];
-                        row++;
-                    }
+                        worksheet.Cell(_text[i].rowIndex + 1, _text[i].columnIndex + 1).Value = _text[i].value;
                 }
 
                 workbook.SaveAs(stream);
@@ -45,7 +50,9 @@ namespace Export.Xlsx
 
         public void SetCellValue(int rowIndex, int columnIndex, string value)
         {
-            _text[rowIndex, columnIndex] = value;
+            //_text[rowIndex, columnIndex] = value;
+            _text.Add((rowIndex, columnIndex, value));
+
         }
 
     }
