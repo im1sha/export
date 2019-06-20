176b8290a788fcef619bc69133f4c7bac2a7e840
im1sha <mikefoxx2000@ya.ru>
2019-06-20 18:08:27 +0300

    Demo.Web: Start implementation

---
+++ Export/Demo.Web/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using Demo.Web.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Demo.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        PersonContext _db;
+        public HomeController()
+        {
+            _db = new PersonContext();
+        }
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public ActionResult Index2()
+        {
+            var db = _db.People.ToList();
+            return View(db);
+        }
+
+        [HttpPost]
+        public ActionResult Index2(Person person)
+        {
+
+            return View();
+        }
+    }
+}
---
+++ Export/Demo.Web/Models/PersonContext.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Demo.Web.Models
+{
+    public class PersonContext : DbContext
+    {
+        public PersonContext() : base("PersonDB")
+        {
+        }
+
+        public DbSet<Person> People { get; set; }
+
+    }
+    public class PersonInitializer : DropCreateDatabaseIfModelChanges<PersonContext>
+    {
+        protected override void Seed(PersonContext context)
+        {
+            var people = new List<Person>()
+            {
+                new Person{ Name="User1",Surname="Teste1" },
+                new Person{ Name="User2",Surname="Teste2" }
+            };
+
+            people.ForEach(p => context.People.Add(p));
+            context.SaveChanges();
+        }
+
+    }
+
+
+}
